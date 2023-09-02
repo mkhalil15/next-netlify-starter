24f9365b3648e5a10e6e85df3019ae6c43e8bcc6
Marwan Khalil <khalilmarwan725@gmail.com>
2023-09-02 17:30:27 -0400

    api integration

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,21 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home() {
+  const [month, setMonth] = useState("");
+  let fetchMonth = React.useCallback(async () => {
+    const response = await axios.get("https://mkhalil.pythonanywhere.com");
+    console.log(response.data);
+  },[])
+
+  useEffect(() => {
+    fetchMonth();
+  }, [fetchMonth])
+
   return (
     <div className="container">
       <Head>
